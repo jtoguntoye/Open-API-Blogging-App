a8a5da63ac964644a6b26b77f9abe2850f6fe747
Joel Oguntoye <joeloguntoye@gmail.com>
2020-11-02 16:32:23 +0100

    Setting up RecyclerView Adapter with AsyncDiffer

--- app/src/main/java/com/codingwithmitch/openapi/ui/main/blog/BlogFragment.kt
+++ app/src/main/java/com/codingwithmitch/openapi/ui/main/blog/BlogFragment.kt
@@ -6,15 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
 import com.codingwithmitch.openapi.R
+import com.codingwithmitch.openapi.models.BlogPost
 import com.codingwithmitch.openapi.ui.main.blog.state.BlogStateEvent
+import com.codingwithmitch.openapi.util.TopSpacingItemDecoration
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_blog.*
 import timber.log.Timber
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class BlogFragment : BaseBlogFragment(){
+class BlogFragment : BaseBlogFragment(), BlogListAdapter.Interaction{
 
+    @Inject
+    lateinit var requestManager: RequestManager
+
+    private lateinit var recyclerAdapter: BlogListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,12 +37,40 @@ class BlogFragment : BaseBlogFragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        goViewBlogFragment.setOnClickListener {
-            findNavController().navigate(R.id.action_blogFragment_to_viewBlogFragment)
-        }
+
+        initRecyclerView()
         subscribeObservers()
         executeSearch()
 
+    }
+
+    fun initRecyclerView(){
+        blog_post_recyclerview.apply {
+            layoutManager = LinearLayoutManager(this@BlogFragment.context)
+
+            val topSpacingDecorator = TopSpacingItemDecoration(30)
+            removeItemDecoration(topSpacingDecorator)  // does nothing if not applied already
+            addItemDecoration(topSpacingDecorator)
+
+            recyclerAdapter = BlogListAdapter(requestManager, this@BlogFragment)
+
+
+            addOnScrollListener(object : RecyclerView.OnScrollListener(){
+                // this will be used for setting up pagination later
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                    val lastPosition = layoutManager.findLastVisibleItemPosition()
+                    if(lastPosition == recyclerAdapter.itemCount.minus(1)){
+                        Timber.d("BlogFragment, Attempting to load next page...")
+                        //TODO ("Load next page ")
+                    }
+
+
+                }
+            })
+            adapter = recyclerAdapter
+        }
     }
 
     fun executeSearch(){
@@ -58,6 +96,23 @@ class BlogFragment : BaseBlogFragment(){
 
         blogViewModel.viewState.observe(viewLifecycleOwner, Observer {viewState ->
             Timber.d("BlogFragment ViewState: ${viewState}")
+            if(viewState != null) {
+                recyclerAdapter.submitList(
+                    viewState.blogFields.blogList,
+                    true
+                )
+            }
         })
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //clear references to prevent memory leaks
+        blog_post_recyclerview.adapter = null
+    }
+
+    override fun onItemSelected(position: Int, item: BlogPost) {
+        Timber.d("onItemSelected: position, BlogPost: $position, ${item}")
     }
 }
